f00a4d4c7f017096c9c0faaa3c5d72825a32c18e
ilgar222 <ilqar6117@gmail.com>
2023-04-07 23:13:14 +0400

    first commit attached

---
+++ src/main/java/lesson03/StudentApp.java
@@ -0,0 +1,12 @@
+package lesson03;
+
+import java.util.Arrays;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        Student samir = new Student(20, 76);
+        System.out.println(samir.getAge());
+        System.out.println(samir.getGrade());
+        System.out.println();
+    }
+}

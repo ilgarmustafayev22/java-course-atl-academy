7643eab501450b7f0a1a03a9cc8dd6c86df92fd2
ilgar222 <ilqar6117@gmail.com>
2023-04-07 23:11:35 +0400

    first commit attached

---
+++ src/main/java/lesson02/P3/Figure.java
@@ -0,0 +1,19 @@
+package lesson02.P3;
+
+public class Figure {
+    private double area;
+    private double perimeter;
+
+    public Figure(double area, double perimeter) {
+        this.area = area;
+        this.perimeter = perimeter;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+}

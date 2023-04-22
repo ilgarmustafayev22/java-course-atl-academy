ce10bf1243b7486060231392df27225c59bd1046
ilgar222 <ilqar6117@gmail.com>
2023-04-22 17:23:43 +0400

    first commit attached

---
+++ src/main/java/draft01/CompareApp.java
@@ -0,0 +1,27 @@
+package draft01;
+
+import java.util.Scanner;
+
+public class CompareApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(compare(a,b,c));
+    }
+
+    private static int compare(int a, int b, int c) {
+        int max = 0;
+        if (max < a) {
+            max = a;
+        }
+        if (max < b) {
+            max = b;
+        }
+        if (max < c) {
+            max = c;
+        }
+        return max;
+    }
+}

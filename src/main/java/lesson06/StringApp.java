434cc8d891c1a661bb05feed9a7eb74c73302439
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-04-27 16:13:57 +0400

    Happy family task attached.

---
+++ src/main/java/lesson06/StringApp.java
@@ -0,0 +1,12 @@
+package lesson06;
+
+import java.nio.charset.StandardCharsets;
+
+public class StringApp {
+    public static void main(String[] args) {
+        String name="Elvin";
+        System.out.println(name.toString());
+        System.out.println(name.toCharArray());
+        System.out.println(name.charAt(0));
+    }
+}

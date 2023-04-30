5c4e73c11044b11fe2e9e286ef6932948c337089
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-04-30 21:55:40 +0400

    code attached

---
+++ src/main/java/lesson07/WrapperApp.java
@@ -0,0 +1,31 @@
+package lesson07;
+
+import java.math.BigDecimal;
+
+public class WrapperApp {
+    public static void main(String[] args) {
+        User elvin = new User("Elvin");
+        System.out.println(elvin);
+        Integer i = new Integer(5); //boxing
+        Integer i2 = Integer.valueOf(6);
+        System.out.println(i);
+        System.out.println(i2);
+
+        Integer a = 5; // autoboxing
+        System.out.println(a);
+        //a = null;
+
+        int b = a; //unboxing
+        System.out.println(b);
+
+       int i1 = Integer.parseInt("123");
+
+       Integer num = 500;
+       Long aLong = Long.valueOf(num);
+       Short aShort = Short.valueOf((short) ((int)num));
+
+        System.out.println(0.2 + 0.2 + 0.2);
+        BigDecimal aNum = BigDecimal.valueOf(0.2);
+        System.out.println(aNum.add(aNum).add(aNum));
+    }
+}

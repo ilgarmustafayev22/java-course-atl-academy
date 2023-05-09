2bca68c3fc2937d5696515b34f1f8ebe4e80c9e6
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-05-09 19:13:35 +0400

    code attached

---
+++ src/main/java/lesson14/LinearSearchApp.java
@@ -0,0 +1,29 @@
+package lesson14;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class LinearSearchApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(nums));
+
+        System.out.println(findNum(num, nums));
+    }
+
+    public static Optional<Integer> findNum(int num, int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (num == nums[i]) {
+                return Optional.of(i);
+            }
+        }
+        return Optional.empty();
+    }
+}

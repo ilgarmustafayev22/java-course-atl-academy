be34e39d237c72d9f80106a408b7c53b04b973b5
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-05-11 19:52:49 +0400

    code attached

---
+++ src/main/java/lesson15/ArrayListApp.java
@@ -0,0 +1,40 @@
+package lesson15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ArrayListApp {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            nums.add((int) (Math.random() * 100));
+        }
+        System.out.println(nums);
+
+        Arrays.sort(new ArrayList[]{nums});
+        System.out.println(nums);
+
+        Collections.sort(nums);
+        System.out.println(nums);
+
+        nums.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.compare(o2, o1);
+            }
+        });
+        System.out.println(nums);
+
+        System.out.println(nums.size());
+
+        nums.remove(2);
+        System.out.println(nums);
+
+        nums.clear();
+        System.out.println(nums);
+
+    }
+}

00091ba537b3d89ffc9a4b94fc65a8bb6dc13816
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-05-11 19:55:41 +0400

    code attached

--- src/main/java/lesson15/ArrayListApp.java
+++ src/main/java/lesson15/ArrayListApp.java
@@ -14,9 +14,6 @@ public class ArrayListApp {
         }
         System.out.println(nums);
 
-        Arrays.sort(new ArrayList[]{nums});
-        System.out.println(nums);
-
         Collections.sort(nums);
         System.out.println(nums);
 

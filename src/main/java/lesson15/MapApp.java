7587cc15c1e84a8f86736a0c0bbecce52a088c4f
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-05-12 20:54:33 +0400

    code attached

---
+++ src/main/java/lesson15/MapApp.java
@@ -0,0 +1,27 @@
+package lesson15;
+
+import java.util.HashMap;
+
+public class MapApp {
+
+    public static void main(String[] args) {
+        HashMap<String, String> people = new HashMap<>();
+        people.put("Elvin", "123");
+        people.put("Samir", "456");
+        people.put("Samir", "123");
+        people.put(null, "123");
+        people.put(null, "56789");
+
+        System.out.println(people);
+
+        boolean elvin = people.containsKey("Elvin");
+        System.out.println(elvin);
+
+        boolean value = people.containsValue("123");
+        System.out.println(value);
+
+        people.clear();
+        System.out.println(people);
+
+    }
+}

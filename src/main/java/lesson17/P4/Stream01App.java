05cb5a745db8ea8a8fd6ae4d24ee6ca17a36962c
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-05-19 20:27:15 +0400

    code attached

--- src/main/java/lesson17/P4/Stream01App.java
+++ src/main/java/lesson17/P4/Stream01App.java
@@ -7,8 +7,25 @@ public class Stream01App {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        list.add("Baku");
+        list.add("Ali");
+        list.add("Ramin");
+        list.add("Kamil");
+
         list
-        .forEach(System.out::println);
+                .stream()
+                .forEach(System.out::println);
+
+        list
+                .stream()
+                .forEach(s -> System.out.println(s.length()));
+        list
+                .stream()
+                .filter(s -> s.length()>3)
+                .forEach(System.out::println);
+        list
+                .stream()
+                .filter(s -> s.length()>=3)
+                .forEach(s -> System.out.println());
     }
+
 }

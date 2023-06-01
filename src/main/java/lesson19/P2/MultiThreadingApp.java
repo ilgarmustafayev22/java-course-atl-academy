cc6dd11eb6622712633e1c25e241ca57be8eb4ed
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-06-01 20:16:36 +0400

    code attached

--- src/main/java/lesson19/P2/MultiThreadingApp.java
+++ src/main/java/lesson19/P2/MultiThreadingApp.java
@@ -3,8 +3,17 @@ package lesson19.P2;
 public class MultiThreadingApp {
 
     public static void main(String[] args) {
-        MultiThreading multiThreading = new MultiThreading();
-        multiThreading.run();
+        Thread01 thread01 = new Thread01();
+        thread01.run();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.MIN_PRIORITY);
+            }
+        }).start();
+
+         new Thread(() -> System.out.println(Thread.MAX_PRIORITY)).start();
     }
 
 }
--- src/main/java/lesson19/P2/Thread01.java
+++ src/main/java/lesson19/P2/Thread01.java
@@ -1,6 +1,6 @@
 package lesson19.P2;
 
-public class MultiThreading extends Thread{
+public class Thread01 implements  Runnable{
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {

123c0df6bd84d5ba31da0ef19f58a4ae7d3c9bc3
ilgarmustafayev22 <126610106+ilgarmustafayev22@users.noreply.github.com>
2023-07-06 20:08:27 +0400

    code attached

--- src/main/java/lesson21/person.sql
+++ src/main/java/lesson21/person.sql
@@ -4,7 +4,7 @@ create table person
     first_name varchar(10) not null ,
     last_name varchar(10) not null ,
     gender varchar(5) not null ,
-    date_of_birth date not null
+    date_of_birth date check ( date_of_birth > 1800 )
 );
 
 insert into person(first_name, last_name, gender, date_of_birth)
